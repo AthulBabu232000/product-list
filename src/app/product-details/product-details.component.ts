72d3fda808883954732a233fd26e7b7dc88af921
webdesign4alll <190048@tkmce.ac.in>
2022-05-06 02:36:07 +0530

    added shipping component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Product, products } from '../products';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+import { CartService } from "../cart.service";
+import { Product, products } from "../products";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"],
 })
 export class ProductDetailsComponent implements OnInit {
-  product?:Product;
-  index:number=0;
+  product?: Product;
+  index: number = 0;
 
-  constructor(private route:ActivatedRoute) {
-  this.route.params.subscribe((params:Params)=>{
-      this.index=params['productId'];
-    
-     this.product=products.find(p=>p.id==this.index);
+  constructor(private route: ActivatedRoute, private cartService: CartService) {
+    this.route.params.subscribe((params: Params) => {
+      this.index = params["productId"];
+
+      this.product = products.find((p) => p.id == this.index);
     });
-   }
-
-  ngOnInit(): void {
-   
+  }
+  addToCart(product:Product){
+    this.cartService.addToCart(product);
+    window.alert("your cart is added");
+    
   }
 
+  ngOnInit(): void {}
 }
